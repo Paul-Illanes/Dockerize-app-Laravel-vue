bf13dfd7ffc82284bfd627a56025d0aed4ca9d82
abelnexus <easywash17@gmail.com>
2022-05-20 10:28:04 -0500

    ignore axios

--- resources/js/src/libs/axios.js
+++ resources/js/src/libs/axios.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  // baseURL: 'http://localhost:8000/',
+  baseURL: 'http://localhost:8000/',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
